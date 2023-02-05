db671a1893074d6e78a130c42e0ee53e699aec6d
draden <hanshengliang@outlook.com>
2023-02-05 16:56:28 +0800

    HTML 5 Video Player

--- src/pages/Demo3/Demo3.jsx
+++ src/pages/Demo3/Demo3.jsx
@@ -1,17 +1,40 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import ReactPlayer from "react-player";
 
 export const Demo3Page = () => {
 
+
+
+    function playPause() {
+        var myVideo = document.getElementById("video1");
+        if (myVideo.paused)
+            myVideo.play();
+        else
+            myVideo.pause();
+    }
+
+    function makeBig() {
+        var myVideo = document.getElementById("video1");
+        myVideo.width = 560;
+    }
+
+    function makeSmall() {
+        var myVideo = document.getElementById("video1");
+        myVideo.width = 320;
+    }
+
+    function makeNormal() {
+        var myVideo = document.getElementById("video1");
+        myVideo.width = 420;
+    }
+
+
     return (
         <>
-            <ReactPlayer
-                width={"100%"}
-                height={"100%"}
-                url='https://player.vimeo.com/video/795140716?keyboard=0'
-                controls={true}
-                onEnded={() => alert("Video ended")}
-            />
+            <button onClick={playPause}>PLAY/PAUSE</button>
+            <video id={"video1"}>
+                <source src="https://master-setup-redcash.hardcore-carver.42-1-60-64.plesk.page/redCash/admin/master/api/v1.0/file/1a65ec57-0ecf-4771-8cac-61f2d34999fe" type="video/mp4" />
+            </video>
         </>
     );
 }
